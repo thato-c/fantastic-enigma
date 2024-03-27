0647e1f968efd11abbb75b27065a8f341d32e391
Thato <t.chitja369@gmail.com>
2024-03-27 14:34:34 +0200

    Set up the Application

---
+++ WelcomePerson/WelcomePerson/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WelcomePerson.Models;
+
+namespace WelcomePerson.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(Person person)
+        {
+            if (person == null)
+            {
+                var newPerson = new Person();
+                return View(newPerson);
+            }
+
+            return View(person);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePerson(Person person)
+        {
+            if (person.Name == null)
+            {
+                person.Name = "Unknown";
+            }
+
+            return RedirectToAction("Index", person);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WelcomePerson/WelcomePerson/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace WelcomePerson.Models
+{
+    public class Person
+    {
+        public string Name { get; set; } = "Unknown";
+
+        public string Message { get; set; } = string.Empty;
+    }
+}
